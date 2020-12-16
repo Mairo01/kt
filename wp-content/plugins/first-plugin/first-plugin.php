c25c3af392dbd9ff5970505804d439e2f84b68dd
Mairo <mairo.planken@khk.ee>
2020-12-16 16:18:57 +0200

    modified plugin `m First Plugin` version 1.0 (after activation of `m First Plugin` version 1.0)

---
+++ wp-content/plugins/first-plugin/first-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: m First Plugin
+ * Plugin URI: http://mairoplanken.ikt.khk.ee
+ * Description: My first plugin
+ * Version: 1.0
+ * Author: M.Planken
+ * Author URI: http://mairoplanken.ikt.khk.ee
+**/
